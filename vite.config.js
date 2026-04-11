068c2f8823ee1a9e79240a0011603b4741f046cf
Zachary Zhuo <zachary.zhuo@opennet.tw>
2026-04-11 17:01:07 +0800

    feat[38;2;102;102;102m:[39m[38;2;187;187;187m [39minit[38;2;187;187;187m [39mvite[38;2;187;187;187m [39m[38;2;102;102;102m+[39m[38;2;187;187;187m [39mreact[38;2;187;187;187m [39mproject[38;2;187;187;187m [39mscaffold
    Add[38;2;187;187;187m [39mVite[38;2;187;187;187m [39m[38;2;102;102;102m+[39m[38;2;187;187;187m [39mReact[38;2;187;187;187m [39mSPA[38;2;187;187;187m [39mscaffold[38;2;187;187;187m [39m[38;2;170;34;255;01mwith[39;00m[38;2;187;187;187m [39mTailwind[38;2;187;187;187m [39mCSS[38;2;102;102;102m,[39m[38;2;187;187;187m [39mVitest[38;2;102;102;102m,[39m[38;2;187;187;187m [39mand[38;2;187;187;187m [39mReact[38;2;187;187;187m [39mRouter[38;2;102;102;102m.[39m
    Replaces[38;2;187;187;187m [39mold[38;2;187;187;187m [39mCRA[38;2;187;187;187m [39mindex[38;2;102;102;102m.[39m[38;2;187;68;68mhtml[39m[38;2;187;187;187m [39mentry[38;2;187;187;187m [39mpoint[38;2;102;102;102m;[39m[38;2;187;187;187m [39mold[38;2;187;187;187m [39mCRA[38;2;187;187;187m [39msub[38;2;102;102;102m-[39mprojects[38;2;187;187;187m [39muntouched[38;2;102;102;102m.[39m

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setup.js'],
+    globals: true,
+  },
+})
